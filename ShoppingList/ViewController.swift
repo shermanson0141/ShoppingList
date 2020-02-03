518b2cbe568ad91d3a385cbd5fc837c82b627303
Severin Hermanson <shermanson0141@stu.d214.org>
2020-02-03 11:20:16 -0600

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Severin on 2/3/20.
+//  Copyright © 2020 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
