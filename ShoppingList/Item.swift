2ed8b63e70580e2797c244fcc208f3be4962fcba
Severin Hermanson <shermanson0141@stu.d214.org>
2020-02-04 11:10:01 -0600

    got tableview working and created item files

---
+++ ShoppingList/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Severin on 2/4/20.
+//  Copyright © 2020 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var newItemTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
     @IBAction func whenButtonPressed(_ sender: Any) {
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 }
 
